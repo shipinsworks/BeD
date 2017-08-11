c5d4357a790b9f9dc31a9c288c2dead8be6ef46d
shipinsworks <takashi.s.nakamura@gmail.com>
2017-08-11 22:05:38 +0900

    debug option

--- bed/sv/macro.svh
+++ bed/sv/macro.svh
@@ -1,6 +1,10 @@
 `ifndef _MACRO_SVH_
  `define _MACRO_SVH_
 
+`ifdef DEBUG
  `define debug_printf( msg ) $display( "[Debug   ] %8d : %s", $stime, $sformatf( msg ))
+`else
+ `define debug_printf( msg )
+`endif
 
 `endif
