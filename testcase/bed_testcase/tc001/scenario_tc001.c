b9827c25ebd21716eed221601bd0ac289ca2d118
shipinsworks <takashi.s.nakamura@gmail.com>
2017-08-15 17:55:11 +0900

    data file read

--- testcase/bed_testcase/tc001/scenario_tc001.c
+++ testcase/bed_testcase/tc001/scenario_tc001.c
@@ -1,21 +1,28 @@
 #include "stdio.h"
 #include "scenario.h"
-FILE *fp = NULL;
-char filepath[] = "dff_data.txt";
+static FILE *fp = NULL;
+static char filepath[] = "testcase/bed_testcase/tc001/dff_data.txt";
 
 uint32_t dff_get_data( pkt_s *pkt )
 {
+  uint32_t data;
+  debug_printf("dff_get_data called.");
   int ret = 0;
   if( fp == NULL ) {
     if(( fp = fopen(filepath,"r")) == NULL ) {
       printf("Error: Cannot open file(%s).",filepath);
       ret = 2;
+    } else {
+      debug_printf("Info: open file(%s) ok.",filepath);
     }
   }
   if( ret == 0 ) {
-    ret = fscanf( fp, "%d", pkt->data[0] );
+    if((ret = fscanf( fp, "%8x", pkt->data )) != EOF) {
+      debug_printf("get data:%08x",pkt->data[0]);
+    } else {
+      ret = 1; // EOF
+    }
   }
-  //  pkt->data[0] = 0x12345678;
   return ret;
 }
 
